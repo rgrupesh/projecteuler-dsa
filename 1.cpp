05f765de25d955915683d266aae692966cdf97a7
Binish Joshi <beeneez6233@gmail.com>
2020-01-05 19:05:36 -0500

    cpp and py

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int i, sum = 0;
+
+	for(i = 1; i < 1000; i++)
+		if(i % 3 == 0 || i % 5 == 0)
+			sum += i;
+
+	cout << "Sum is " << sum;
+
+	return 0;
+}
