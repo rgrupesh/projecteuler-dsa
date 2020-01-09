dcc0a9ada097ab932bc41db700aca3268beb9f80
Binish Joshi <beeneez6233@gmail.com>
2020-01-09 09:47:22 -0500

    Solved 3

--- 3.c
+++ 3.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
+#include<math.h>
 
 int main()
 {
 	long int i, j, lpf, num = 600851475143, count = 1;
 
-	for(i = 1; i < num; i++)
+	for(i = 1; i < ceil(sqrt(num)); i++) if(num % i == 0)
 	{
-		printf("Fator check with %d\n", i);
-		if(num % i == 0)
-		{
-			for(j = 2, count = 0; j < i; j++)
-				if(i % j == 0) count++;
-			if(count = 0) lpf = j;
-		}
+		for(j = 2, count = 0; j < i; j++) if(i % j == 0) count++;
+		if(count == 0) lpf = j;
 	}
 
-	printf("The largest prime factor is %d", lpf);
+	printf("The largest prime factor is %ld", lpf);
 	return 0;
 }
