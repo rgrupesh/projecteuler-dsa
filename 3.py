87c77a27d6384785dad5ca0c1ab99cc35657c0ea
rgrupesh <54068868+rgrupesh@users.noreply.github.com>
2020-01-10 16:44:12 +0545

    changed count and increased range for loop

--- 3.py
+++ 3.py
@@ -7,10 +7,10 @@ j = 2
 for i in range(1, math.ceil(math.sqrt(num))):
 	if(num % i == 0): # Factor check
 		count = 0
-		for j in range(2, i): #Prime Number check
+		for j in range(2, i+1): #Prime Number check
 			if(i % j == 0):
 				count += 1
-		if(count == 0):
+		if(count == 1):
 			lpf = j
 
 print("Largest Prime factor of " + str(num) + " is " + str(lpf))
